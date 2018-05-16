90f10d8f6cc0fc0b58409b437792aeede1aa504e
Michelle Sri <michelle.sri@gmail.com>
2018-05-16 13:21:42 -0700

    css basics js practice and challenges and started node

--- jsChallenges/js11.js
+++ jsChallenges/js11.js
@@ -1,5 +1,6 @@
 // JS-11 (intermediate): Write a function that has one parameter and returns an array that contains all primes (prime numbers) less than N.
 
+//using HOF
 var isPrime = function(number) {
   for (var i = 2; i < number; i++)
     if (number % i == 0) {
@@ -19,3 +20,42 @@ function primeArr(num, f){
 }
 
 console.log(primeArr(30, isPrime));
+
+// put into one function
+
+function primeArr(num){
+  var isPrime = function(number) {
+    for (var i = 2; i < number; i++)
+      if (number % i == 0) {
+        return false;
+      }
+    return true;
+  };
+  var arrOfPrimes = [];
+  for (var i = 0; i < num; i++){
+    if (isPrime(i)){
+      arrOfPrimes.push(i);
+    }
+  }
+  return arrOfPrimes;
+}
+
+console.log(primeArr(30, isPrime));
+
+// using loops
+
+function primeArr(num){
+  var arrOfPrimes = [];
+  for (var i = 0; i < num; i++){
+    var isPrime = true;
+    for (var j = 2; j < i; j++)
+      if (i % j == 0) {
+        isPrime = false;
+      }
+    if (isPrime){
+      arrOfPrimes.push(i);
+    }
+  }
+  return arrOfPrimes;
+}
+console.log(primeArr(30));
--- jsPractice/app.js
+++ jsPractice/app.js
@@ -1,20 +1,5 @@
-//problem: we need a simple way to look at a user's badge count and js points
-// solution: use node.js to connect to treehouse's api to get profile information to print out
+var profile = require('./profile');
 
-var http = require('http');
-var username = 'michellesrikhundonr';
+var users = ['michellesrikhundonr', 'michaelfreeman5', 'geoffreyemerson', 'aaronbini'];
 
-function printMessage(username, badgeCount, points){
-  var msg = username + ' has ' + badgeCount + ' total badge(s) and ' + points + ' points in js';
-  console.log(msg);
-}
-
-//connect to the api url
-
-var request = http.get('http://teamtreehouse.com/' + username + '.json', function(response){
-  console.log(response.statusCode);
-}).on('error', function(err){
-  console.log('got error: ' + err.message);
-});
-
-printMessage('Michelle', 1000, 2000);
+users.forEach(profile.get);
