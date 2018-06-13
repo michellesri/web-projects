3131d06601426a17f3ad57f9aa9e820c712aaaa4
Michelle Sri <michelle.sri@gmail.com>
2018-06-13 13:09:42 -0700

    data binding in angular on todo list

--- angular/scripts/hello-world.js
+++ angular/scripts/hello-world.js
@@ -1,9 +1,11 @@
-angular.module('todoListApp')
-.directive('helloWorld', function(){
-  return {
-    template: 'This is the hello world directive',
-    restrict: 'E'
-    // this E restricts the hello-world to be an element only (ex. <hello-world>)
-    // A would restrict it to attribute only (ex. <div hello-world>)
-  };
-});
+// this is commented out because the hello-world directive div in the html is commented out
+
+// angular.module('todoListApp')
+// .directive('helloWorld', function(){
+//   return {
+//     template: 'This is the hello world directive',
+//     restrict: 'E'
+//     // this E restricts the hello-world to be an element only (ex. <hello-world>)
+//     // A would restrict it to attribute only (ex. <div hello-world>)
+//   };
+// });
