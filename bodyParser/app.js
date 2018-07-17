8a349d4909809e07cd21bae9c4fb8b4013fe08f9
Michelle Sri <michelle.sri@gmail.com>
2018-07-17 13:44:37 -0700

    added comments to body parser functionality

--- bodyParser/app.js
+++ bodyParser/app.js
@@ -1,3 +1,7 @@
+//need body parser to take the data back, convert it to json, and put it back together.
+  //we need to do this because the bodyParser will handle on and end events and add it to the req object as req.body.
+
+
 function bodyParser(req, res, next){
   var body = '';
   req.on('data', chunk => body += chunk);
