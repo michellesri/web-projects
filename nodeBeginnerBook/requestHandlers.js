cd8356a76b2cce26482a7af873850ef7df012396
Michelle Sri <michelle.sri@gmail.com>
2018-07-25 15:38:25 -0700

    fixed lint spaces

--- nodeBeginnerBook/requestHandlers.js
+++ nodeBeginnerBook/requestHandlers.js
@@ -1,11 +1,10 @@
-const querystring = require('querystring'),
-    fs = require('fs')
-    formidable = require('formidable');
+const fs = require('fs'),
+  formidable = require('formidable');
 
 function start(res){
-    console.log('request handler "start" was called');
+  console.log('request handler "start" was called');
 
-    var body = '<html>' +
+  var body = '<html>' +
     '<head>' +
     '<meta http-equiv="Content-Type" content="text/html; ' +
     'charset=UTF-8" />' +
@@ -19,37 +18,37 @@ function start(res){
     '</body>' +
     '</html>'; 
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(body);
-    res.end();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(body);
+  res.end();
 }
 
 function upload(res, req){
-    console.log('request handler "upload" was called');
+  console.log('request handler "upload" was called');
 
-    var form = new formidable.IncomingForm();
-    console.log('about to parse');
-    form.parse(req, (error, fields, files) => {
-        console.log('parsing done');
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(req, (error, fields, files) => {
+    console.log('parsing done');
 
         // rename function with windows fs fix. windows tried to rename already existing file.
-        fs.rename(files.upload.path, '/tmp/test.png', error => {
-            if (error){
-                fs.unlink('/tmp/test.png');
-                fs.rename(files.upload.path, '/tmp/test.png');
-            }
-        });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('received img: </br>');
-        res.write('<img src="/show"/>');
-        res.end();
+    fs.rename(files.upload.path, '/tmp/test.png', error => {
+      if (error){
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
     });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('received img: </br>');
+    res.write('<img src="/show"/>');
+    res.end();
+  });
 }
 
 function show(res){
-    console.log('request handle "show" was called');
-    res.writeHead(200, {'Content-Type': 'image/png'});
-    fs.createReadStream('/tmp/test.png').pipe(res);
+  console.log('request handle "show" was called');
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(res);
 }
 
 exports.start = start;
--- nodeBeginnerBook/server.js
+++ nodeBeginnerBook/server.js
@@ -1,15 +1,16 @@
 var http = require('http');
 var url = require('url');
+var port = 8888 || process.argv[2];
 
 function start(route, handle){
-    function onRequest(req, res){
-        var pathname = url.parse(req.url).pathname;
-        console.log('request for ' + pathname + ' received');
-        route(handle, pathname, res, req);
-        }
+  function onRequest(req, res){
+    var pathname = url.parse(req.url).pathname;
+    console.log('request for ' + pathname + ' received');
+    route(handle, pathname, res, req);
+  }
 
-        http.createServer(onRequest).listen(8888);
-        console.log('server has started');
- }
+  http.createServer(onRequest).listen(port);
+  console.log('server has started on ' + port);
+}
 
 exports.start = start;
